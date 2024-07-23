fc9782c94d1fb1baeb1f966fefb188eff320c109
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-07-23 14:36:31 -0300

    Add files via upload

---
+++ marco.3e/teste.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá</title>
+</head>
+<body>
+    <h1>Primeiro código PHP</h1>
+    <?php
+        echo "Olá, mundo!";
+    ?>
+</body>
+</html>

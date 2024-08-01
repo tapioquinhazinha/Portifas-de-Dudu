4981ea8399037ece847410fa9c254da767cc7c13
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-08-01 14:45:18 -0300

    Create Aula06.php

---
+++ marco.3e/Aula06.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>Aula 06 php</title>
+</head>
+<body>
+    <?php 
+    echo "<h1>Arrays - Vetores</h1>";
+    $carros = array('monza','santana','camaro','uno');
+    echo $carros[1], "<br>";
+    echo $carros[3], "<br>";
+
+
+
+
+    ?>
+</body>
+</html>

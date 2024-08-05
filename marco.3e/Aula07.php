5c166a47e3ccc4acd3ab061ea82c12fe08e3627d
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-08-05 18:19:25 -0300

    Create Aula07.php

---
+++ marco.3e/Aula07.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="recebe-aula07,2.php?nome=Eduardo">Enviar o nome Eduardo</a>
+
+    <form action="recebe-aula07,2.php">
+        <label for="nome">Nome: </label>
+        <input type="text" name="nome"><br>
+        <label for="sobrenome">Sobrenome: </label>
+        <input type="text" name="sobrenome"><br>
+        <input type="submit" value="Enviar">
+
+    </form>
+    
+</body>
+</html>

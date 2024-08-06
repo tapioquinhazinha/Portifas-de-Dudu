8bd924714315e514f2d39eb9a9586e816b119bd1
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-08-06 16:59:56 -0300

    Create Aula08.php

---
+++ marco.3e/Aula08.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <form action="recebe-aula08.php" method="post">
+        <label for="nome">Nome: </label><br>
+        <input type="text" name="nome"><br>
+        <label for="sobrenome">Sobrenome: </label><br>
+        <input type="text" name="sobrenome"><br>
+        <input type="submit" value="Enviar">
+
+    </form>
+    
+</body>
+</html>

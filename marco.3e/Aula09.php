a3659f084b2f1eab3d52e67f26c7133e9b16ba65
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-08-15 14:50:13 -0300

    Create Aula09.php

---
+++ marco.3e/Aula09.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+   <form action="calcular.php" method="post">
+    <label for="n1">numero 1: </label>
+    <input type="text" name="n1" ><br>
+    <label for="n2">numero 2: </label>
+    <input type="text" name="n2"><br>
+    <input type="submit" value="Somar"><br>
+    resultado: <input type="text" width="5"><br>
+
+   </form>
+ 
+ 
+
+</body>
+</html>

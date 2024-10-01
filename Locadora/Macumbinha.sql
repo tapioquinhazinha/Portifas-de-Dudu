5e85ad223e9c683f8e46cd73c86943610e2f3284
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-10-01 14:58:20 -0300

    Add files via upload

---
+++ Locadora/Macumbinha.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 01-Out-2024 às 20:01
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca`
+--
+CREATE DATABASE IF NOT EXISTS `biblioteca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `biblioteca`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `emprestimos`
+--
+
+CREATE TABLE `emprestimos` (
+  `CodEmprestimo` int(11) NOT NULL,
+  `Data_Emprestimo` date DEFAULT NULL,
+  `Data_Devolução` date DEFAULT NULL,
+  `CodLivro` int(11) DEFAULT NULL,
+  `CodLeitor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `emprestimos`
+--
+
+INSERT INTO `emprestimos` (`CodEmprestimo`, `Data_Emprestimo`, `Data_Devolução`, `CodLivro`, `CodLeitor`) VALUES
+(1, '2023-06-14', '2023-06-16', 1, 1),
+(2, '2023-06-15', '2023-12-15', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `leitores`
+--
+
+CREATE TABLE `leitores` (
+  `CodLeitor` int(11) NOT NULL,
+  `Nome` varchar(255) DEFAULT NULL,
+  `DtNasc` date DEFAULT NULL,
+  `Celular` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `RA` int(11) DEFAULT NULL,
+  `Endereco` varchar(255) DEFAULT NULL,
+  `NumEnd` varchar(255) DEFAULT NULL,
+  `Bairro` varchar(255) DEFAULT NULL,
+  `CidadeUF` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `leitores`
+--
+
+INSERT INTO `leitores` (`CodLeitor`, `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES
+(1, 'João Silva Sauro', '2001-04-01', '(11)98788-2311', 'joao@hotmail.com', 631113101, 'Rua Boa Esperança', '201', 'Janga', 'Paulista/PE'),
+(2, 'Maria Silva Nascimento', '2011-07-21', '(11)98788-2311', 'maria@hotmail.com', 631113102, 'Rua da Vovó', '101', 'MAranguape I', 'Paulista/PE'),
+(3, 'Filipe Luis', '2010-09-19', '(87)9778-6667', 'g2a@gmail.com', 999666777, 'Rua do Mudo', '09', 'Inferno', 'Uberlândia'),
+(4, 'Felipe Leonardo', '0000-00-00', '(90)97776-2312', 'leona2gmail.com', 2147483647, 'Rua do Cão', '69', 'Felipe', 'Leonardo'),
+(5, 'Juju do Piquici', '0000-00-00', '(00)97878-2424', 'AAAAAgmail.com', 909090, 'CHIGXONGA', '00', 'Chinaland', 'Petroleia/SP');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `CodLivro` int(11) NOT NULL,
+  `Titulo` varchar(255) DEFAULT NULL,
+  `Autor` varchar(255) DEFAULT NULL,
+  `Editora` varchar(255) DEFAULT NULL,
+  `Sinopse` varchar(255) DEFAULT NULL,
+  `AnoPublicacao` int(11) DEFAULT NULL,
+  `Genero` varchar(255) DEFAULT NULL,
+  `Paginas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `livros`
+--
+
+INSERT INTO `livros` (`CodLivro`, `Titulo`, `Autor`, `Editora`, `Sinopse`, `AnoPublicacao`, `Genero`, `Paginas`) VALUES
+(1, 'Biblia', 'Abraham Kuyper', 'KJA', NULL, 2015, 'Religioso', 950),
+(2, 'One Piece', 'Eichiro Oda', 'Toei', NULL, 1997, 'Ação', 21000);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD PRIMARY KEY (`CodEmprestimo`),
+  ADD KEY `CodLivro` (`CodLivro`),
+  ADD KEY `CodLeitor` (`CodLeitor`);
+
+--
+-- Índices para tabela `leitores`
+--
+ALTER TABLE `leitores`
+  ADD PRIMARY KEY (`CodLeitor`);
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`CodLivro`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  MODIFY `CodEmprestimo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `leitores`
+--
+ALTER TABLE `leitores`
+  MODIFY `CodLeitor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `CodLivro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD CONSTRAINT `emprestimos_ibfk_1` FOREIGN KEY (`CodLivro`) REFERENCES `livros` (`CodLivro`),
+  ADD CONSTRAINT `emprestimos_ibfk_2` FOREIGN KEY (`CodLeitor`) REFERENCES `leitores` (`CodLeitor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

786d4135f6f5219d3beb0b43d65d2a51161dc851
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-10-07 18:21:01 -0300

    Create cadastra-leitores.php

---
+++ Locadora/cadastra-leitores.php
@@ -0,0 +1,27 @@
+?php
+include "config.php";
+
+if(!$conn){
+    die("Falha na Conexão {mysqli_connect_error()}");
+
+    $nome = "";
+
+    $dtnasc = "";
+
+    $celular = "";
+
+    $email = "";
+
+    $ra = "";
+
+    $endereco = "";
+
+    $num_end = "";
+
+    $bairro = "";
+
+    $cidade = "";
+
+    $sql = "";
+
+}

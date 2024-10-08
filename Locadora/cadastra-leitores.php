52e607cb434e84cc8e816b62a5585fafdc212e89
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-10-08 14:46:06 -0300

    Update cadastra-leitores.php

--- Locadora/cadastra-leitores.php
+++ Locadora/cadastra-leitores.php
@@ -24,4 +24,11 @@ if(!$conn){
 
     $sql = "";
 
+
+  $sql ="INSERT INTO `leitores`
+    (`Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES 
+    ('$nome','$dtnasc','$celular','$email','$ra','$endereco','$num_end','$bairro','$cidade')";
+
+$query = mysqli_query(mysql: $conn,query: $sql) or
+die(mysqli_error(mysql: $db));
 }

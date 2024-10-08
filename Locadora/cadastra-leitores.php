e59c7d648ed6fe294cd7a012877da3a36052f234
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-10-08 17:52:00 -0300

    Update cadastra-leitores.php

--- Locadora/cadastra-leitores.php
+++ Locadora/cadastra-leitores.php
@@ -3,33 +3,43 @@ include "config.php";
 
 if(!$conn){
     die("Falha na Conexão {mysqli_connect_error()}");
+}
 
-    $nome = "";
+$nome = "Teste Nome";
 
-    $dtnasc = "";
+$dtnasc = "1900-12-01";
 
-    $celular = "";
+$celular = "(81) 999998888";
 
-    $email = "";
+$email = "jsdjnwa@gmail.com";
 
-    $ra = "";
+$ra = "09091289";
 
-    $endereco = "";
+$endereco = "Rua Teste";
 
-    $num_end = "";
+$num_end = "99";
 
-    $bairro = "";
+$bairro = "Revorve";
 
-    $cidade = "";
-
-    $sql = "";
+$cidade = "Paulista/PE";
 
 
-  $sql ="INSERT INTO `leitores`
-    (`Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES 
-    ('$nome','$dtnasc','$celular','$email','$ra','$endereco','$num_end','$bairro','$cidade')";
-    
+$sql ="INSERT INTO `leitores`
+(`Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES 
+('$nome','$dtnasc','$celular','$email','$ra','$endereco','$num_end','$bairro','$cidade')";
 
 $query = mysqli_query(mysql: $conn,query: $sql) or
 die(mysqli_error(mysql: $db));
+
+if($query){
+    echo "<center>";
+    echo "Cadastro Realizado Com Sucesso!!<br>";
+    echo "<a href='index.php'><button title='Home page'>Voltar</button></a>";
+    echo "</center>";
+}else{
+    echo "<center>";
+    echo "Erro ao Cadastrar!!<br>";
+    echo "<a href='index.php'><button title='Home page'>Voltar</button></a>";
+    echo "</center>";
 }
+?>

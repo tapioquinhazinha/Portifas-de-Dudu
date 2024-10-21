13b350100c5c7ab1aa39a266e43d43456ff66e6c
tapioquinhazinha <166050464+tapioquinhazinha@users.noreply.github.com>
2024-10-21 17:20:14 -0300

    Create cadastra-livros.php

---
+++ marco.3e/Supermegabiblotecal/cadastra-livros.php
@@ -0,0 +1,42 @@
+<?php
+include "config.php";
+
+if(!$conn){
+    die("Falha na Conexão" . mysqli_connect_error());
+}
+
+$Titulo = "$_POST[Titulo]";
+
+$Autor = "$_POST[Autor]";
+
+$Editora = "$_POST[Editora]";
+
+$Sinopse = "$_POST[Sinopse]";
+
+$AnoPublicacao = "$_POST[AnoPublicacao]";
+
+$Genero = "$_POST[Genero]";
+
+$Paginas = "$_POST[Paginas]";
+
+$sql = "INSERT INTO `livros`
+(`Titulo`, `Autor`, `Editora`, `Sinopse`, `AnoPublicacao`, `Genero`, `Paginas`)
+VALUES 
+('$Titulo','$Autor','$Editora','$Sinopse','$AnoPublicacao','$Genero','$Paginas')";
+
+
+
+$query = mysqli_query(mysql: $conn,query: $sql) or
+die(mysqli_error(mysql: $conn));
+
+if($query){
+    echo "<center>";
+    echo "Cadastro Realizado Com Sucesso!!<br>";
+    echo "<a href='cadastrar livro.html'><button title='Home page'>Voltar</button></a>";
+    echo "</center>";
+}else{
+    echo "<center>";
+    echo "Erro ao Cadastrar!!<br>";
+    echo "<a href='cadastrar livro.html'><button title='Home page'>Voltar</button></a>";
+    echo "</center>";
+}
